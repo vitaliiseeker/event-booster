364482057b63f862eb7c1ba07db80008fd9cb984
anastasiia-matushenko <muzaam07@gmail.com>
2022-09-16 09:21:10 +0300

    added class eventsApi

---
+++ src/js/modules/eventsApi.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const baseRequest = axios.create({
+    baseURL: "https://app.ticketmaster.com/discovery/v2/",
+    params: {
+        apikey: "w8His6LGv1GwvDEPMoHLiyBhemCG7Mij",
+    }
+});
+
+export class eventsApi {
+    static page = 0;
+    static totalPages = null;
+    static size = 16;
+    // static query = "";
+    
+    static async fetchEvents(query, country) {
+        const endPoint = "events.json";
+
+        const config = {
+            params: {
+                keyword: query,
+                countryCode: country,
+                size: eventsApi.size,
+                page: eventsApi.page,
+            }
+        }
+
+        const searchResult = await baseRequest.get(endPoint, config);
+        const response = await searchResult.data._embedded.events;
+        return response;
+    }
+};
