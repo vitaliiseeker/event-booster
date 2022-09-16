a4cf22e30689a4bdb1503ae0bd9d0d40d62bac4c
serheiev <69833095+serheiev@users.noreply.github.com>
2022-09-16 14:22:07 +0300

    add bg, change card

--- src/js/modules/eventsApi.js
+++ src/js/modules/eventsApi.js
@@ -1,39 +1,38 @@
-import axios from "axios";
+import axios from 'axios';
 
 const baseRequest = axios.create({
-    baseURL: "https://app.ticketmaster.com/discovery/v2/",
-    params: {
-        apikey: "w8His6LGv1GwvDEPMoHLiyBhemCG7Mij",
-    }
+  baseURL: 'https://app.ticketmaster.com/discovery/v2/',
+  params: {
+    apikey: 'w8His6LGv1GwvDEPMoHLiyBhemCG7Mij',
+  },
 });
 
 export class eventsApi {
-    static page = 0;
-    static totalPages = null;
-    static size = 16;
-    // static query = "";
+  static page = 0;
+  static totalPages = null;
+  static size = 16;
+  // static query = "";
 
   /**
-   * 
+   *
    * @param {string} query "" (if you don't pass the argument)
-   * @param {string} country 
+   * @param {string} country
    * @returns result response (array);
-   */  
-    static async fetchEvents(query = "", country = "") {
-        const endPoint = "events.json";
+   */
+  static async fetchEvents(query = '', country = '') {
+    const endPoint = 'events.json';
 
-        const config = {
-            params: {
-                keyword: query,
-                countryCode: country,
-                size: eventsApi.size,
-                page: eventsApi.page,
-            }
-        }
+    const config = {
+      params: {
+        keyword: query,
+        countryCode: country,
+        size: eventsApi.size,
+        page: eventsApi.page,
+      },
+    };
 
-        const searchResult = await baseRequest.get(endPoint, config);
-        const response = await searchResult.data._embedded.events;
-        return response;
-    }
-};
-
+    const searchResult = await baseRequest.get(endPoint, config);
+    const response = await searchResult.data._embedded.events;
+    return response;
+  }
+}
