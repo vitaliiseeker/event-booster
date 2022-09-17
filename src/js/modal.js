09cd40aa1c95a8a2ef659dfe529588657f57cce2
DmytroPopko <popkomu@gmail.com>
2022-09-17 23:53:01 +0300

    modify modal footer & footer scss. make close modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,13 @@
 import './modules/eventsApi';
 // import code from './data/countries.json';
 
+export function openCloseModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
+
+export function isKeyPressed(evt) {
+  if (evt.code === 'Escape') {
+    refs.modal.classList.toggle('is-hidden');
+    // document.removeEventListener('keydown', isKeyPressed);
+  }
+}
