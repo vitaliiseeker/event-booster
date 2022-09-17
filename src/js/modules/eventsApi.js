e02869bdff6e5aa68805fd180bebd2f2cafab175
serheiev <69833095+serheiev@users.noreply.github.com>
2022-09-17 14:00:16 +0300

    fix card markup

--- src/js/modules/eventsApi.js
+++ src/js/modules/eventsApi.js
@@ -1,89 +1,85 @@
 import axios from 'axios';
 
 const baseRequest = axios.create({
-    baseURL: "https://app.ticketmaster.com/",
-    params: {
-        apikey: "w8His6LGv1GwvDEPMoHLiyBhemCG7Mij",
-    }
+  baseURL: 'https://app.ticketmaster.com/',
+  params: {
+    apikey: 'w8His6LGv1GwvDEPMoHLiyBhemCG7Mij',
+  },
 });
 
 export class EventsApi {
-    static page = 0;
-    static totalPages = null;
-    static size = 16;
-    static baseEndPoint = "discovery/v2/";
+  static page = 0;
+  static totalPages = null;
+  static size = 16;
+  static baseEndPoint = 'discovery/v2/';
 
-    static links = {};
-    // static query = "";
+  static links = {};
+  // static query = "";
 
   /**
    *
    * @param {string} query "" (if you don't pass the argument)
    * @param {string} country
    * @returns result response (array);
-   */  
-    static async fetchEvents(query = "", country = "") {
-        const endPoint = `${EventsApi.baseEndPoint}events.json`;
+   */
+  static async fetchEvents(query = '', country = '') {
+    const endPoint = `${EventsApi.baseEndPoint}events.json`;
 
-        const config = {
-            params: {
-                keyword: query,
-                countryCode: country,
-                size: EventsApi.size,
-                page: EventsApi.page,
-            }
-        }
+    const config = {
+      params: {
+        keyword: query,
+        countryCode: country,
+        size: EventsApi.size,
+        page: EventsApi.page,
+      },
+    };
 
-        return await EventsApi.checkResponse(endPoint, config);
+    return await EventsApi.checkResponse(endPoint, config);
+  }
+
+  static async fetchEventsDefault() {
+    const endPoint = `${EventsApi.baseEndPoint}events.json`;
+
+    const config = {
+      params: {
+        keyword: 'piano',
+        countryCode: 'US',
+        size: EventsApi.size,
+      },
+    };
+
+    return await EventsApi.checkResponse(endPoint, config);
+  }
+
+  static async fetchEventsById(id) {
+    const endPoints = `${EventsApi.baseEndPoint}events/${id}`;
+
+    const searchResult = await baseRequest.get(endPoints);
+    const response = await searchResult.data;
+    return response;
+  }
+
+  static async checkResponse(endPoint, config) {
+    const searchResult = await baseRequest.get(endPoint, config);
+    const response = await searchResult.data;
+
+    if (!response._embedded) {
+      throw new Error('Oops, not found!');
     }
 
-    static async fetchEventsDefault() {
-        const endPoint = `${EventsApi.baseEndPoint}events.json`;
+    const responseEvents = response._embedded.events;
+    // console.log(responseEvents);
+    return responseEvents;
+  }
 
-        const config = {
-            params: {
-                keyword: "piano",
-                countryCode: "US",
-                size: EventsApi.size,
-            }
-        }
+  static async getNextPage() {
+    // console.log(EventsApi.links);
+    const nextLink = await baseRequest.get(`${EventsApi.links.next.href}`);
+    return nextLink;
+  }
 
-        return await EventsApi.checkResponse(endPoint, config);
-    }
-
-    static async fetchEventsById(id) {
-        const endPoints = `${EventsApi.baseEndPoint}events/${id}`;
-
-        const searchResult = await baseRequest.get(endPoints);
-        const response = await searchResult.data;
-        return response;
-    }
-
-    static async checkResponse(endPoint, config) {
-        const searchResult = await baseRequest.get(endPoint, config);
-        const response = await searchResult.data;
-
-        if (!response._embedded) {
-            throw new Error("Oops, not found!");
-        }
-
-        const responseEvents = response._embedded.events;
-        // console.log(responseEvents);
-        return responseEvents;        
-    }
-
-    static async getNextPage() {
-        // console.log(EventsApi.links);
-        const nextLink = await baseRequest.get(`${EventsApi.links.next.href}`);
-        return nextLink;
-    }
-    
-    static async getPrevPage() {
-        const prevLink = await baseRequest.get(`${EventsApi.links.prev.href}`);
-        return prevLink;
-    }
-};
-
-
-
-
+  static async getPrevPage() {
+    const prevLink = await baseRequest.get(`${EventsApi.links.prev.href}`);
+    return prevLink;
+  }
+}
