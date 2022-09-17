ad11a5d9ce62fb138d8dbd98ccbba1cb139730de
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-17 11:19:20 +0300

    update select

--- src/js/search.js
+++ src/js/search.js
@@ -1,64 +1,70 @@
 import { eventsApi, response } from './modules/eventsApi';
 
-// import axios from 'axios';
+import code from './data/countries.json';
 
-// const baseRequest = axios.create({
-//   baseURL: 'https://app.ticketmaster.com/discovery/v2/',
-//   params: {
-//     apikey: 'w8His6LGv1GwvDEPMoHLiyBhemCG7Mij',
-//   },
-// });
+import axios from 'axios';
 
-// export class eventsApi {
-//   static page = 0;
-//   static totalPages = null;
-//   static size = 16;
-//   static query = '';
+const baseRequest = axios.create({
+  baseURL: 'https://app.ticketmaster.com/discovery/v2/',
+  params: {
+    apikey: 'w8His6LGv1GwvDEPMoHLiyBhemCG7Mij',
+  },
+});
 
-//   /**
-//    *
-//    * @param {string} query "" (if you don't pass the argument)
-//    * @param {string} country
-//    * @returns result response (array);
-//    */
-//   static async fetchEvents(query = '', country = '') {
-//     const endPoint = 'events.json';
+export class eventsApi {
+  static page = 0;
+  static totalPages = null;
+  static size = 16;
+  static query = '';
 
-//     const config = {
-//       params: {
-//         keyword: query,
-//         countryCode: country,
-//         size: eventsApi.size,
-//         page: eventsApi.page,
-//       },
-//     };
+  /**
+   *
+   * @param {string} query "" (if you don't pass the argument)
+   * @param {string} country
+   * @returns result response (array);
+   */
+  static async fetchEvents(query = '', country = '') {
+    const endPoint = 'events.json';
 
-//     const searchResult = await baseRequest.get(endPoint, config);
-//     const response = await searchResult.data._embedded.events;
+    const config = {
+      params: {
+        keyword: query,
+        countryCode: country,
+        size: eventsApi.size,
+        page: eventsApi.page,
+      },
+    };
 
-//     return response;
-//   }
-// }
+    const searchResult = await baseRequest.get(endPoint, config);
+    const response = await searchResult.data._embedded.events;
+
+    return response;
+  }
+}
 
 const searchBox = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search-input');
+const selectCountry = document.querySelector('.select-country');
+const selectInput = document.querySelector('.select-country-js');
+const valuecountry = document.querySelector('.value-country');
+const selectBtn = document.querySelector('.select-btn-js');
+
 //const searchBtn = document.querySelector('.search-btn');
+
+console.log(valuecountry);
 
 searchBox.addEventListener('submit', onSearch);
 
 function onSearch(e) {
   e.preventDefault();
   const searchBoxValue = searchInput.value;
-  // console.log(searchBoxValue);
-  if (searchBoxValue === '') {
-    console.log('Please, input event!');
-    return;
-  }
 
+  // if (searchBoxValue === '') {
+  //   console.log('Please, input event!');
+  //   return;
+  // }
   // eventsApi.query = e.currentTarget.elements.searchInput.value;
-
   //   const resultEvents = eventsApi.fetchEvents(searchBoxValue);
-  //   console.log(resultEvents);
 
   eventsApi
     .fetchEvents(searchBoxValue)
@@ -67,8 +73,50 @@ function onSearch(e) {
       console.log('Show ooops!');
     })
     .finally(() => console.log('Promise settled clear input????'));
-
-  function renderEvents(events) {
-    console.log(events);
-  }
 }
+
+function renderEvents(events) {
+  // рендер
+  console.log(events);
+}
+
+//--------add country to select
+selectBtn.addEventListener('click', onSelect);
+
+function onSelect() {
+  let z = document.getElementById('select').value;
+  console.log(z);
+
+  let searchCountryValue = selectCountry.value;
+  console.log(searchCountryValue);
+
+  eventsApi
+    .fetchEvents('', 'ca')
+    .then(events => console.log(events))
+    .catch(error => {
+      console.log('Show ooops!');
+    })
+    .finally(() => console.log('Promise settled clear input????'));
+}
+
+const markupSelect = code
+  .map(el => `<option value="${el.code}">${el.name}</option>`)
+  .join('');
+
+selectCountry.insertAdjacentHTML('beforeend', markupSelect);
+selectInput.value = selectCountry.value;
+
+selectCountry.addEventListener('change', aaa);
+function aaa() {
+  selectInput.value = selectCountry.value;
+  // console.log(selectCountry.outerText);
+}
+
+selectInput.addEventListener('input', event => {
+  let a = event.currentTarget.value;
+  console.log(a);
+});
+
+//console.log(valueCountry.textContent);
+
+//var value = select.options[sel.selectedIndex].value;
