7a85a7a481a2d1b23a36226af11c606a9d48438b
DmytroPopko <popkomu@gmail.com>
2022-09-18 15:19:56 +0300

    modify footer js, add decor img

--- src/js/footer.js
+++ src/js/footer.js
@@ -18,11 +18,13 @@ function isKeyPressed(evt) {
 function openModal() {
   refs.modal.classList.remove('is-hidden');
   document.addEventListener('keydown', isKeyPressed);
+  document.body.classList.toggle('scrollOff');
 }
 
 function closeModal() {
   refs.modal.classList.add('is-hidden');
   document.removeEventListener('keydown', isKeyPressed);
+  document.body.classList.toggle('scrollOff');
 }
 
 document.addEventListener('click', function (e) {
@@ -35,3 +37,20 @@ document.addEventListener('click', function (e) {
     closeModal();
   }
 });
+
+// const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+//   const body = document.body;
+//   body.style.position = 'fixed';
+//   body.style.top = `-${scrollY}`;
+// };
+// const closeDialog = () => {
+//   const body = document.body;
+//   const scrollY = body.style.top;
+//   body.style.position = '';
+//   body.style.top = '';
+//   window.scrollTo(0, parseInt(scrollY || '0') * -1);
+//   document.getElementById('dialog').classList.remove('show');
+// }
+// window.addEventListener('scroll', () => {
+//   document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+// });
