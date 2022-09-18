0d7cb586dc0aab69bd38dc02dd4119f622f37871
anastasiia-matushenko <muzaam07@gmail.com>
2022-09-18 19:08:41 +0300

    update method class, markup modal

--- src/js/modules/markupEventModal.js
+++ src/js/modules/markupEventModal.js
@@ -2,9 +2,40 @@ export function createMarkupEventModal(arr) {
     return arr.reduce((acc, {
         name,
         info,
-        dates: { start },
-    }) => 
-    acc + `<div class="card-modal">
+        images,
+        dates: { start, timezone }, 
+        priceRanges,
+        url,
+        _embedded: { venues },
+    }) => {
+        let strInfo = "text description";
+        if (info) {
+            strInfo = info;
+        }
+        let strPriceList = ""; 
+        priceRanges.forEach(element => {
+            let priceBox = "price-box";
+            let btnVip = "";
+            let nameType = "Standard";
+            
+            if (element.type.toLowerCase() === "vip") {
+                nameType = "VIP";
+                priceBox += "-vip";
+                btnVip = "btn-vip";
+            }
+
+            strPriceList += `<p class="price-box${priceBox}">
+                                <span class="event-icon-ticket">
+                                    <svg class="icon icon-ticket" width="24" height="16">
+                                        <use href="./images/symbol.svg#icon-ticket"></use>
+                                    </svg>
+                                </span>
+                                <span>${nameType} ${element.min}-${element.max} ${element.currency}</span>
+                            </p>
+                            <a class="btn-buy-tickets ${btnVip}" href="${url}">BUY TICKETS</a>`
+        });
+
+     return acc + `<div class="card-modal">
                 <button class="card-modal_close" data-modal-close>
                     <svg class="card-modal_close-svg" width="17" height="17">
                         <use href="./images/symbol.svg#icon-close"></use>
@@ -12,24 +43,24 @@ export function createMarkupEventModal(arr) {
                 </button>
 
                 <div class="card-modal_box-img">
-                    <img class="card-modal_img-small" src="" alt="">
+                    <img class="card-modal_img-small" src="${images[3].url}" alt="">
                 </div>
                 <div class="card-modal_box-info">
-                    <img class="card-modal_img-original" src="" alt="">
+                    <img class="card-modal_img-original" src="${images[0].url}" alt="">
                     <ul class="card-modal_list">
                         <li class="card-modal_info">
                             <h3 class="card-modal_title">INFO</h3>
-                            <p>${info}</p>
+                            <p>${strInfo}</p>
                         </li>
                         <li class="card-modal_info">
                             <h3 class="card-modal_title">WHEN</h3>
                             <p>${start.localDate}</p>
-                            <p>${start.localTime}</p>
+                            <p>${start.localTime.slice(0, -3)} (${timezone})</p>
                         </li>
                         <li class="card-modal_info">
                             <h3 class="card-modal_title">WHERE</h3>
-                            <p></p>
-                            <p></p>
+                            <p>${venues[0].country.name}</p>
+                            <p>${venues[0].name}</p>
                         </li>
                         <li class="card-modal_info">
                             <h3 class="card-modal_title">WHO</h3>
@@ -37,29 +68,12 @@ export function createMarkupEventModal(arr) {
                         </li>
                         <li class="card-modal_info">
                             <h3 class="card-modal_title">PRICES</h3>
-                            <p>
-                                <span class="event-icon-ticket">
-                                    <svg class="icon icon-ticket" width="24" height="16">
-                                        <use href="./images/symbol.svg#icon-ticket"></use>
-                                    </svg>
-                                </span>
-                                <span></span>
-                            </p>
-                            <button class="btn-buy-tickets" type="button">BUY TICKETS</button>
-                            <p class="price-box-vip">
-                                <span class="event-icon-ticket">
-                                    <svg class="icon icon-ticket" width="24" height="16">
-                                        <use href="./images/symbol.svg#icon-ticket"></use>
-                                    </svg>
-                                </span>
-                                <span></span>
-                            </p>
-                            <button class="btn-buy-tickets btn-vip" type="button">BUY TICKETS</button>
+                            ${strPriceList}
                         </li>
                     </ul>
                 </div>
-                <button class="btn-more-info" type="button">More about this event</button>
+                 <a class="btn-more-info" href="">More about this event</a>        
             </div>`
-    , "");
+    }, "");
 };
 
