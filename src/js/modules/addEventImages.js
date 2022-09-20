e951fbc8daae8d830147b968b5b633dfecfffabc
anastasiia-matushenko <muzaam07@gmail.com>
2022-09-20 17:15:24 +0300

    fixed modal, class

--- src/js/modules/addEventImages.js
+++ src/js/modules/addEventImages.js
@@ -1,14 +1,16 @@
 export function addEventImages(arr) {
+    const minSize = 480;
+    const maxSize = 1280;
 
-    if (document.body.offsetWidth <= 480) {
+    if (document.body.offsetWidth <= minSize) {
         return arr.find((item) => item.url.includes("ARTIST_PAGE_3_2"))
     }
 
-    if (document.body.offsetWidth > 480 && document.body.offsetWidth < 1200) {
+    if (document.body.offsetWidth > minSize && document.body.offsetWidth < maxSize) {
         return arr.find(item => item.url.includes("RETINA_PORTRAIT_3_2"))
     }
 
-    if (document.body.offsetWidth >= 1200) {
+    if (document.body.offsetWidth >= maxSize) {
         return arr.find(item => item.url.includes("TABLET_LANDSCAPE_16_9"))
     }
 }
